9f5bcabe8f3701f81bab7996279c1069cb8d9a85
pb9536 <pb9536@intl.att.com>
2018-05-08 16:28:25 +0530

    Update twilio_1.py

--- twilio_1.py
+++ twilio_1.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACa9e89d50c1ffd9310294a3eed7bf6b01"
+account_sid = "Ac SID"
 # Your Auth Token from twilio.com/console
-auth_token  = "5d13d272aadce9b376d6a3aa7bf2cc24"
+auth_token  = "Token"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+919985197278", 
+    to="+your number", 
     from_="+15753668089",
     body="Hello from Python!")
 
